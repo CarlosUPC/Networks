822c7be08191e51b802467e789eb11840e4ab84b
CarlosPeña <36167348+CarlosUPC@users.noreply.github.com>
2020-11-26 14:30:12 +0100

    implementing client replication manager

--- Multiplayer Game/ReplicationManagerClient.cpp
+++ Multiplayer Game/ReplicationManagerClient.cpp
@@ -1,3 +1,44 @@
 #include "Networks.h"
 
 // TODO(you): World state replication lab session
+
+void ReplicationManagerClient::read(const InputMemoryStream& packet)
+{
+	uint32 networkId;
+	ReplicationAction action;
+	
+	packet >> networkId;
+	packet >> action;
+
+	if (action == ReplicationAction::Create)
+	{
+		GameObject* gameObject = Instantiate();
+		if (gameObject != nullptr)
+		{
+			App->modLinkingContext->registerNetworkGameObjectWithNetworkId(gameObject, networkId);
+
+			packet >> gameObject->position.x;
+			packet >> gameObject->position.y;
+			packet >> gameObject->angle;
+		}
+	}
+	else if (action == ReplicationAction::Update)
+	{	
+		GameObject* gameObject = App->modLinkingContext->getNetworkGameObject(networkId);
+		if (gameObject != nullptr)
+		{
+			packet >> gameObject->position.x;
+			packet >> gameObject->position.y;
+			packet >> gameObject->angle;
+		}
+
+	}
+	else if (action == ReplicationAction::Destroy)
+	{
+		GameObject* gameObject = App->modLinkingContext->getNetworkGameObject(networkId);
+		if (gameObject != nullptr)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
