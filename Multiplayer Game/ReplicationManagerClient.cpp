2b99c185ef378780506154fc87847fe2cabe943b
CarlosPeña <36167348+CarlosUPC@users.noreply.github.com>
2020-11-28 11:57:48 +0100

    forgot loop on packet replication read

--- Multiplayer Game/ReplicationManagerClient.cpp
+++ Multiplayer Game/ReplicationManagerClient.cpp
@@ -4,42 +4,47 @@
 
 void ReplicationManagerClient::read(const InputMemoryStream& packet)
 {
-	uint32 networkId;
-	ReplicationAction action;
-	
-	packet >> networkId;
-	packet >> action;
 
-	if (action == ReplicationAction::Create)
+	while (packet.RemainingByteCount > 0)
 	{
-		GameObject* gameObject = Instantiate();
-		if (gameObject != nullptr)
-		{
-			App->modLinkingContext->registerNetworkGameObjectWithNetworkId(gameObject, networkId);
+		uint32 networkId;
+		ReplicationAction action;
 
-			packet >> gameObject->position.x;
-			packet >> gameObject->position.y;
-			packet >> gameObject->angle;
-		}
-	}
-	else if (action == ReplicationAction::Update)
-	{	
-		GameObject* gameObject = App->modLinkingContext->getNetworkGameObject(networkId);
-		if (gameObject != nullptr)
+		packet >> networkId;
+		packet >> action;
+
+		if (action == ReplicationAction::Create)
 		{
-			packet >> gameObject->position.x;
-			packet >> gameObject->position.y;
-			packet >> gameObject->angle;
+			GameObject* gameObject = Instantiate();
+			if (gameObject != nullptr)
+			{
+				App->modLinkingContext->registerNetworkGameObjectWithNetworkId(gameObject, networkId);
+
+				packet >> gameObject->position.x;
+				packet >> gameObject->position.y;
+				packet >> gameObject->angle;
+			}
+		}
+		else if (action == ReplicationAction::Update)
+		{
+			GameObject* gameObject = App->modLinkingContext->getNetworkGameObject(networkId);
+			if (gameObject != nullptr)
+			{
+				packet >> gameObject->position.x;
+				packet >> gameObject->position.y;
+				packet >> gameObject->angle;
+			}
+
+		}
+		else if (action == ReplicationAction::Destroy)
+		{
+			GameObject* gameObject = App->modLinkingContext->getNetworkGameObject(networkId);
+			if (gameObject != nullptr)
+			{
+				App->modLinkingContext->unregisterNetworkGameObject(gameObject);
+				Destroy(gameObject);
+			}
 		}
 
-	}
-	else if (action == ReplicationAction::Destroy)
-	{
-		GameObject* gameObject = App->modLinkingContext->getNetworkGameObject(networkId);
-		if (gameObject != nullptr)
-		{
-			App->modLinkingContext->unregisterNetworkGameObject(gameObject);
-			Destroy(gameObject);
-		}
 	}
 }
